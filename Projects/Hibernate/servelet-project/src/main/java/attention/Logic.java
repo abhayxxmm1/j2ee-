7368758a9a9ad18345f7380a28fe747db4622f48
Abhay <ghongadeabhay60@gmail.com>
2026-01-04 23:41:48 +0530

    Achieved Desired file structure

--- Projects/Hibernate/servelet-project/src/main/java/attention/Logic.java
+++
@@ -1,8 +0,0 @@
-package attention;
-
-public class Logic {
-
-	private static String URl = "jdbc:postgres://localhost:5432/un";
-	private static String USN = "postgres";
-	private static String PWD = "root";
-}
